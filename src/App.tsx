9d9cf33c880026b2a44bcfa593676198e039e689
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-04 14:52:13 -0500

    create useMultistepForm custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import { useMultistepForm } from "./useMultistepForm"
+
 function App() {
+  const {
+    steps,
+    currentStepIndex,
+    step
+  } = useMultistepForm([<div>One</div>, <div>Two</div>])
+
   return (
-    <h1>Hi</h1>
+    <div
+      style={{
+        position: 'relative',
+        background: 'white',
+        border: '1px solid black',
+        padding: '2rem',
+        margin: '1rem',
+        borderRadius: '.5rem',
+        fontFamily: 'Arial'
+      }}
+    >
+      <form>
+        <div style={{
+            position: 'absolute',
+            top: '.5rem',
+            right: '.5rem'
+          }}
+        >
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+      </form>
+    </div>
   )
 }
 
