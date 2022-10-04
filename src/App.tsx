8d404044c65651832bd188d7923f2daa9f79951a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-04 15:15:28 -0500

    create FormWrapper component for style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { useMultistepForm } from "./useMultistepForm"
+import { AccountForm } from "./AccountForm"
+import { AddressForm } from "./AddressForm"
+import { UserForm } from "./UserForm"
 
 function App() {
   const {
@@ -9,7 +12,7 @@ function App() {
     isLastStep,
     back,
     next
-  } = useMultistepForm([<div>One</div>, <div>Two</div>])
+  } = useMultistepForm([<UserForm />, <AddressForm />, <AccountForm />])
 
   return (
     <div
